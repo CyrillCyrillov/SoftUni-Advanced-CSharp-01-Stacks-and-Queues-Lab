9bd286bd489950b6cbea6dac19b207d2411b3825
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 23:00:21 +0200

    Add files via upload

---
+++ Task06_Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+
+            while (true)
+            {
+                string rawComand = Console.ReadLine();
+
+                if(rawComand.ToUpper() == "END")
+                {
+                    break;
+                }
+
+                if(rawComand.ToUpper() == "PAID")
+                {
+                    while (customers.Count > 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(rawComand);
+                }
+            }
+            
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
