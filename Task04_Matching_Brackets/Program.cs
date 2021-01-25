9150156754d62731eada21509b6eeb1ed89888a9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 22:58:34 +0200

    Add files via upload

---
+++ Task04_Matching_Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task04_Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arithmeticExpression = Console.ReadLine();
+            Stack<int> indexBrackets = new Stack<int>();
+            int startIndex = 0;
+
+            for (int i = 0; i <= arithmeticExpression.Length - 1; i++)
+            {
+                if(arithmeticExpression[i] == '(')
+                {
+                    indexBrackets.Push(i);
+                }
+
+                if(arithmeticExpression[i] == ')')
+                {
+                    startIndex = indexBrackets.Pop();
+                    //string substring = arithmeticExpression.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(arithmeticExpression.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
