af428535f542dbbe1525a09955601bc5714dee7d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 22:52:31 +0200

    Add files via upload

---
+++ Task01_Reverse_Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01_Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            Stack<char> reverseStringStack = new Stack<char>();
+
+            for (int i = 0; i <= inputString.Length - 1 ; i++)
+            {
+                reverseStringStack.Push(inputString[i]);
+            }
+
+            //int stackSize = reverseStringStack.Count;
+
+            while (reverseStringStack.Count > 0)
+            {
+                Console.Write(reverseStringStack.Pop());
+            }
+            
+            
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
