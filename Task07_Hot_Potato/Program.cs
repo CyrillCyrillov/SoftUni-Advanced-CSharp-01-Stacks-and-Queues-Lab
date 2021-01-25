0aa3b26470978a3a4d84d2f2c783c34dde28238c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 23:01:11 +0200

    Add files via upload

---
+++ Task07_Hot_Potato/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task07_Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNames = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Queue<string> names = new Queue<string>();
+            foreach (string name in inputNames)
+            {
+                names.Enqueue(name);
+            }
+
+            int countNumber = int.Parse(Console.ReadLine());
+
+            while (names.Count > 1)
+            {
+                for (int i = 1; i < countNumber; i++)
+                {
+                    names.Enqueue(names.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {names.Dequeue()}");
+            }
+            
+            
+            Console.WriteLine($"Last is {names.Dequeue()}");
+        }
+    }
+}
