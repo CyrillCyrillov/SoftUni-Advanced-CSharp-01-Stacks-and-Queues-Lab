bb01c87109051860c2186b86d32f5d66e1cb837d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 22:56:18 +0200

    Add files via upload

---
+++ Task03_Simple_Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task03_Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mathStringInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Stack<string> mathStack = new Stack<string>();
+            foreach (string element in mathStringInput.Reverse())
+            {
+                mathStack.Push(element.ToString());
+            }
+
+            while (mathStack.Count > 1)
+            {
+                int firstNumber = int.Parse(mathStack.Pop());
+                string operation = mathStack.Pop();
+                int secondNumber = int.Parse(mathStack.Pop());
+
+                int result = 0;
+                if(operation == "+")
+                {
+                    result = firstNumber + secondNumber;
+                }
+                
+                if(operation == "-")
+                {
+                    result = firstNumber - secondNumber;
+                }
+
+                mathStack.Push(result.ToString());
+            }
+            
+            Console.WriteLine(mathStack.Pop());
+        }
+    }
+}
