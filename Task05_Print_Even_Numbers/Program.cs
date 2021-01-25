54e74a5635439c412a0dbf96e9d4ce2490ebb7b7
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-25 22:59:30 +0200

    Add files via upload

---
+++ Task05_Print_Even_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task05_Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> numbersQueue = new Queue<int>();
+
+            foreach (int element in numbers)
+            {
+                numbersQueue.Enqueue(element);
+            }
+
+            while (numbersQueue.Count > 0)
+            {
+                int nextNumber = numbersQueue.Dequeue();
+                if(nextNumber % 2 == 0)
+                {
+                    Console.Write(nextNumber);
+                    if(numbersQueue.Count > 0)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
